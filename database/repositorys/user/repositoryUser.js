6290069d5920948e6cffdabfae0613c91167df22
Yago Fogaca <yagofogaca20@gmail.com>
2022-09-27 14:20:24 -0300

    feat: User repository creation

---
+++ database/repositorys/user/repositoryUser.js
@@ -0,0 +1,23 @@
+import { usersDB } from "../../mongo/schemas/schemaUser/schemaUser";
+
+export class RepositoryUsers {
+  static async create(user) {
+    return await usersDB.create(user);
+  }
+
+  static async delete(userID) {
+    return await usersDB.deleteOne({ id: userID });
+  }
+
+  static async getByEmail(userEmail) {
+    return await usersDB.findOne({ email: userEmail });
+  }
+
+  static async getById(userId) {
+    return await usersDB.findOne({ id: userId });
+  }
+
+  static async update(user) {
+    return await usersDB.findOneAndUpdate({ userId: user.id }, user, { new: true });
+  }
+}
