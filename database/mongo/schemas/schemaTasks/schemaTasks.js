ac71ddd4a9ee9854a413b24aa7c605601fdb0867
Yago Fogaca <yagofogaca20@gmail.com>
2022-09-27 13:20:36 -0300

    feat: Updated task modeling, removed description and added day of the week and

--- database/mongo/schemas/schemaTasks/schemaTasks.js
+++ database/mongo/schemas/schemaTasks/schemaTasks.js
@@ -3,11 +3,10 @@ import { Schema, model } from "mongoose";
 const schemaTask = new Schema({
   id: { type: String, required: true },
   id_user: { type: String, required: true },
-  day: { type: String, required: true },
+  day_week: { type: String, required: true },
   hour: { type: String, required: true },
   task: { type: String, required: true },
-  status: { type: String, required: true },
-  description: { type: String, required: false },
+  status: { type: Boolean, required: true },
 });
 
 export const tasksDB = model("Taks", schemaTask);
--- entitys/tasksEntity/tasks.Entity.js
+++ entitys/tasksEntity/tasks.Entity.js
@@ -4,17 +4,14 @@ export class TaskEntity {
   constructor(task) {
     this.id = task.id ?? Validate.shortId();
     this.id_user = task.id_user;
-    this.day = task.day;
     this.day_week = task.day_week;
     this.hour = task.hour;
     this.task = task.task;
     this.status = task.status;
-    this.description = task.description ?? "";
   }
 
-  validateTask(taks) {
-    console.log(taks);
-    if (!this.id_user || !this.day || !this.hour || !this.task || !this.status) {
+  validateTask() {
+    if (!this.id_user || !this.day_week || !this.hour || !this.task || !this.status.toString()) {
       throw new Error("Some fields were not reported");
     }
   }
@@ -23,11 +20,10 @@ export class TaskEntity {
     return {
       id: this.id,
       id_user: this.id_user,
-      day: this.day,
       hour: this.hour,
       task: this.task,
       status: this.status,
-      description: this.description,
+      day_week: this.day_week,
     };
   }
 }
