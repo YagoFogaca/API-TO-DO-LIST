c966e17e3b5ae3649b076acfc03094a768b91529
Yago Fogaca <yagofogaca20@gmail.com>
2022-09-27 15:44:28 -0300

    feat: User functionality implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ConnectionDatabase } from "./database/mongo/connection.js";
 import { FactoryTasks } from "./factories/tasks/factorieTasks.js";
+import { FactoryUsers } from "./factories/users/factorieUsers.js";
 import express, { Router } from "express";
 import cors from "cors";
 await ConnectionDatabase.connectionDatabase();
@@ -7,11 +8,13 @@ await ConnectionDatabase.connectionDatabase();
 const router = Router();
 const app = express();
 const tasks = FactoryTasks(router);
+const users = FactoryUsers(router);
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 app.use("/task", tasks.route());
+app.use("/user", users.route());
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
